ccad3b45ff68fe68ebb1609227372709829cc546
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-01-02 10:19:52 +0500

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,13 +1,13 @@
 
 var mongoose = require('mongoose');
-// mongoose.connect(
-//     process.env.DB,
-//     { useNewUrlParser: true })
-//      .then(() => console.log('connected'))
-//      .catch( (error) => error);
+ mongoose.connect(
+     process.env.DB,
+     { useNewUrlParser: true })
+      .then(() => console.log('connected'))
+      .catch( (error) => error);
   
 
-mongoose.connect("mongodb://localhost:27017/my_database",
+/* mongoose.connect("mongodb://localhost:27017/my_database",
     { useNewUrlParser: true })
      .then(() => console.log('connected'))
-     .catch( (error) => error);
+     .catch( (error) => error); */
