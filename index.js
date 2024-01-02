dcd0bd83da1b2dc78a6fdbd91e48f2d28878fb1b
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-01-02 18:27:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const express = require("express");
 require('dotenv').config()
 require("./db/conn");
 const app = express();
+const user = require("./src/routs/user");
+const router = express.Router();
+const login = require("./src/routs/login");    
+const cookieParser = require('cookie-parser');
+const fileUplode = require('express-fileupload');
+const add_post_func = require("./routs/addpost");
+const AddPost = require("./db/schemas/add_post_schema");
+const updatepost = require('./src/routs/updatepost');
+const deleatpost = require('./src/routs/deleatpost');
+const regUser = require('./src/routs/regUser');
+const userdata = require('./src/routs/usersdata');
+const userupdate = require('./src/routs/updateuser');
+const {Server} = require('socket.io');
+const http = require("http");
+// const PORT = process.env.PORT;         
+const getFrontUser = require("./src/routs/getfrontuser.js")
 
+const addUser = require("./db/modules/addusermodule.js");
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
