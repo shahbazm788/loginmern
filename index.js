692d5292e026060503e0f4a8aa42373877db6bbe
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-01-02 18:33:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require('dotenv').config();
-// require("./db/conn");
+const db = require("./db/conn");
 const app = express();
 // const user = require("./src/routs/user");
 // const router = express.Router();
