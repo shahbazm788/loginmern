f62b935fd45125974e790e0d779c0b060af3968d
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-01-03 16:37:18 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(cors({
   credentials: true,
-      origin: ["https://loginmern-admin.vercel.app","https://blog-woad-chi-43.vercel.app"],
+      origin: ["https://loginmern-admin.vercel.app","https://loginmern-cslp.vercel.app"],
       methods: ["GET", "POST", "PUT", "DELETE"],
      
     })
