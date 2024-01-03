5a997a101ecebac957d05d221b12ec6d256492ba
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-01-03 15:37:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,87 @@ app.use(cors({
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
+app.use(cookieParser());
+app.use(fileUplode());
+
+
+
+app.use('/updatepost',updatepost);
+
+app.use('/deleatpost',deleatpost);
+
+
+// geting user data 
+app.use('/usersdata',userdata);
+//updateuser 
+
+
+
+
+app.use('/updateuser',userupdate);
+app.post("/login", (req,res) => {
+    login(req,res);
+
+})
+
+
+app.post("/reg", (req,res) => {
+
+    // going on regUser.js file for function called below
+    regUser(req,res);
+  // console.log(req.body); 
+
+});
+
+app.get("/user", (req,res) => {
+  user(req,res);
+})
+
+
+//uploade files 
+app.post("/upload", (req,res) => {
+  let file = req.files.file;
+  let fileName = file.name;
+   const uploadePath = __dirname + './../uploads/' + fileName;
+  //const uploadePath = __dirname + '/../myapp/src/img/' + fileName;
+
+  // console.log(fileName);
+  file.mv(uploadePath,(err) => {
+    if(err){
+      res.send(err) 
+    }
+    else{
+      res.send("succecfully uploded")
+      console.log('uploded')
+    }
+  })
+  
+})
+
+// start add post/ start adding new user from front end
+app.post("/addusers", (req,res) => {
+ addUser(req,res);
+  // console.log(req.body);
+});
+
+
+app.use("/getuser",getFrontUser)
+app.post('/addpost',(req,res) => {
+  add_post_func(req,res)
+
+  
+}) 
+
+
+
+  
+// geting posts 
+app.get("/posts",async (req,res) => {
+    const posts = await AddPost.find();
+    res.send(posts);
+    console.log("getting someone");
+});
+
 
 app.listen(5000, () => {
   console.log("Running on port 5000.");
