e9568a1c5482dbce7d19e595d10bc19fe68a514f
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-01-04 22:04:42 +0500

    Update login.js

--- src/routs/login.js
+++ src/routs/login.js
@@ -14,6 +14,7 @@ const login = async  (req,res,next) => {
                withCredentials: true,
                httpOnly: false,
              });
+        res.send({"jwt":req.cookies.jwt})
        
     }
 else{
