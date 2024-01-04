f4e9a216e169b33331fb538ef0a88d149c1fb099
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-01-04 22:01:46 +0500

    Update login.js

--- src/routs/login.js
+++ src/routs/login.js
@@ -1,72 +1,3 @@
-// const RegistedUser = require("../../db/schemas/userregistor");
-// const express = require("express");
-// const router = express.Router();
-// const jwt = require("jsonwebtoken");
-// const cookies = require("cookie-parser");
-// require("dotenv").config();
-// const bcrypt = require('bcrypt');
-
-
-// const login = async  (req,res,next) => {
-//     if(req.cookies.jwt){
-//         // console.log(res.cookies.jwt);
-//         res.send({"jwt":`${req.cookies.jwt}`});
-//           res.cookie("jwt", req.cookies.jwt, {
-//                withCredentials: true,
-//                httpOnly: false,
-//              });
-//         console.log(req.cookies);
-//     }
-// else{
-
-//     // destructring req.body object
-//     const {email,password} = req.body;
-//        // console.log(req.cookies)
-//     try {
-//         // fiding user by emaill
-//         const getData = await RegistedUser.findOne({email:email});
-//         //geting user id
-//         const _id = getData._id;
-//         const userPass = getData.password;
-
-//      bcrypt.compare(password,userPass, async ( err,result) => {
-//         if(result){
-//                //genrating jwt token
-//                const token = await  getData.genreatToken();
-            
-//                //updating user token
-//                const updatetUser = await RegistedUser.findOneAndUpdate({_id},{$set:{
-//                    tokens: {token}
-//                }},{
-//                    new:true
-//                })
-           
-//                // sending token with cookeis to browser
-//            res.cookie("jwt", token, {
-//                withCredentials: true,
-//                httpOnly: false,
-//              });
-//            // sending respons to clinte side
-//            res.json(updatetUser)
-//             console.log(updateUser);
-//         }
-//         else{
-//             res.send('login failed')
-//         }
-//      })
-       
-//     } catch (e) {
-//         console.error('Error', e)
-//     }
-// }
-    
-// }
-
-
-// module.exports = login;
-
-
-
 const RegistedUser = require("../../db/schemas/userregistor");
 const express = require("express");
 const router = express.Router();
@@ -77,6 +8,15 @@ const bcrypt = require('bcrypt');
 
 
 const login = async  (req,res,next) => {
+    if(req.cookies.jwt){
+       
+          res.cookie("jwt", req.cookies.jwt, {
+               withCredentials: true,
+               httpOnly: false,
+             });
+       
+    }
+else{
 
     // destructring req.body object
     const {email,password} = req.body;
@@ -117,8 +57,11 @@ const login = async  (req,res,next) => {
     } catch (e) {
         console.error('Error', e)
     }
+}
     
 }
 
 
 module.exports = login;
+
+
